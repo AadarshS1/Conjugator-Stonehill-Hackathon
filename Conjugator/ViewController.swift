ebd2f4542228d714045ea9e22f58dd89b5c35a37
Karthik Narayanan <karthiknarayanan@Karthiks-MacBook-Pro-2.local>
2018-02-26 14:42:27 +0530

    Update

--- Conjugator/ViewController.swift
+++ Conjugator/ViewController.swift
@@ -55,7 +55,7 @@ class ViewController: UIViewController {
     }
     
     func displayText(){
-        answerBox.text = getRawVerb()
+        answerBox.text = getPrefix() + " " + getRawVerb() + (suffix)! + "."
     }
     
     var finalWord: String?
